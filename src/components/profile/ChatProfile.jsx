e5a8b831a523a0b2a4069972b8efee8a720bd279
unknown <engr.adnanhossain@gmail.com>
2024-05-14 21:56:39 +0600

    add chat profile

---
+++ src/components/profile/ChatProfile.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ChatProfile = () => {
+    return (
+        <div className='flex flex-col items-center mt-10'>
+            <img className='h-20 w-20 rounded-full' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSScRRIBGaB2SXFbYoR8uS9UGFPBOsqzP21NqiF7QSKcg&s" alt="avatar" />
+            <h3 className='font-medium'>Adnan Hossain</h3>
+            <p className='text-xs text-dark'>Active Now</p>
+        </div>
+    );
+};
+
+export default ChatProfile;
--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -2,12 +2,15 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from '../shared/Sidebar';
 import NavigationSidebar from '../shared/NavigationSidebar';
+import ChatProfile from '../components/profile/ChatProfile';
 
 const MainLayout = () => {
     return (
         <div className='2xl:container 2xl:mx-auto'>
-            <div className='grid grid-cols-7'>
-                <div className='col-span-2 grid grid-cols-6'>
+            <div className='grid grid-cols-10'>
+
+                {/* Navigation Sidebar  */}
+                <div className='col-span-3 grid grid-cols-6'>
                     <div className='col-span-1'>
                         <NavigationSidebar />
                     </div>
@@ -15,8 +18,15 @@ const MainLayout = () => {
                         <Sidebar />
                     </div>
                 </div>
+
+                {/* Chat Container Outlet  */}
                 <div className='col-span-5'>
                     <Outlet />
+                </div>
+
+                {/* Chat Profile  */}
+                <div className='col-span-2'>
+                    <ChatProfile />
                 </div>
             </div>
         </div>
