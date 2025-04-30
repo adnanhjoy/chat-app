a50c4639124e208a08f812444f395efb677a0478
Adnan Hossain <engr.adnanhossain@gmail.com>
2025-04-30 10:48:35 +0600

    add active icon on chat profile

--- src/components/profile/ChatProfile.jsx
+++ src/components/profile/ChatProfile.jsx
@@ -6,7 +6,10 @@ const ChatProfile = ({ singleFriend }) => {
     return (
         <div className='bg-white p-2 border rounded-lg h-full'>
             <div className='flex flex-col items-center mt-10'>
-                <img className='h-20 w-20 rounded-full' src={avatar} alt="avatar" />
+                <div className='relative'>
+                    <img className='h-20 w-20 rounded-full' src={avatar} alt="avatar" />
+                    <p className='bg-primary h-3 w-3 rounded-full absolute right-1.5 bottom-1.5'></p>
+                </div>
                 <h3 className='font-medium'>{name}</h3>
                 <p className='text-xs text-dark'>Active Now</p>
             </div>
